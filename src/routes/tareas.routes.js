b5ce28f0ec8cd2649d454ad19d2eef3500644318
basiliomartinez <basilioezequielmartinez@gmail.com>
2026-01-15 11:38:54 -0300

    feat: aplicamos express-validator en POST y PUT de tareas

--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,5 +1,13 @@
 import { Router } from "express";
-import { prueba, listarTareas, crearTarea, borrarTarea, editarTarea } from "../controllers/tareas.controllers.js";
+import {
+  prueba,
+  listarTareas,
+  crearTarea,
+  borrarTarea,
+  editarTarea,
+} from "../controllers/tareas.controllers.js";
+import validacionTarea from "../helpers/validacionTarea.js";
+import resultadoValidacion from "../helpers/resultadoValidacion.js";
 
 const router = Router();
 
@@ -10,14 +18,12 @@ router.get("/test", prueba);
 router.get("/", listarTareas);
 
 // POST - crear tarea
-router.post("/", crearTarea);
+router.post("/", validacionTarea, resultadoValidacion, crearTarea);
 
 // DELETE - borrar tarea por id
 router.delete("/:id", borrarTarea);
 
 // EDITAR - editar tarea por id
-router.put("/:id", editarTarea);
-
-
+router.put("/:id", validacionTarea, resultadoValidacion, editarTarea);
 
 export default router;
